b3d862209dffa707e30ed12771c69ccd2f49dfdd
Facundo Romano <facundo-romano@hotmail.com>
2022-12-07 17:56:45 -0300

    Third deply The Vanguard
    Better Responsive, still broken in small and medium phones

---
+++ precache-manifest.c38c7e31f66e23e0a8a160773fb1ef01.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "a7412d312d752292af73",
+    "url": "/static/css/main.82b5612f.chunk.css"
+  },
+  {
+    "revision": "a7412d312d752292af73",
+    "url": "/static/js/main.a7412d31.chunk.js"
+  },
+  {
+    "revision": "e270198bf8c9e2993766",
+    "url": "/static/css/1.bf1da215.chunk.css"
+  },
+  {
+    "revision": "e270198bf8c9e2993766",
+    "url": "/static/js/1.e270198b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "23de304d4bd6cfa93caff642bff8bedb",
+    "url": "/static/media/pngegg.23de304d.png"
+  },
+  {
+    "revision": "2000a04a4289c215db390dbab28f82b0",
+    "url": "/static/media/Odinson.2000a04a.ttf"
+  },
+  {
+    "revision": "4c46f624e5b248fdc16386ac968ef65f",
+    "url": "/static/media/asd.4c46f624.jpg"
+  },
+  {
+    "revision": "27e2a469ee8a2753cd45a86f0ee658ae",
+    "url": "/index.html"
+  }
+];
